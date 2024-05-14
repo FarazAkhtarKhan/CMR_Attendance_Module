c4cd0efb5a54915ccd4f3b03591c0a5b24964d47
unknown <faraz.akhtar@cr.edu.in>
2024-05-14 20:57:36 +0530

    vercel config_4

--- CMR_Attendance_Module/settings.py
+++ CMR_Attendance_Module/settings.py
@@ -131,7 +131,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/profile_pic')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
