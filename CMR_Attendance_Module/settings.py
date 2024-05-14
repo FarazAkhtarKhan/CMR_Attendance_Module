15267bd1dbfafeeeb615252a29aa3e26777753de
unknown <faraz.akhtar@cr.edu.in>
2024-05-14 21:50:03 +0530

    vercel config_7

--- CMR_Attendance_Module/settings.py
+++ CMR_Attendance_Module/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
-MEDIA_URL = 'media/'
+MEDIA_URL = 'media/profile_pic/'
 
 MEDIA_ROOT = BASE_DIR/'media'
 # Default primary key field type
