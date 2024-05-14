012af49546aa46cf4a0772e1dda041c80eff7fe9
unknown <faraz.akhtar@cr.edu.in>
2024-05-14 17:30:25 +0530

    vercel hosting

--- CMR_Attendance_Module/settings.py
+++ CMR_Attendance_Module/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vtsi!@ntrfl-_=v#nq!vcx2xu0iu4c#@6pqg=*&)6^xk3-n$*n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
 # Application definition
 
